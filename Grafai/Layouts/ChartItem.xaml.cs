55d53b0eadf78f4ef8829b359d5fd0642eae2e8b
Zylius <zylius1@gmail.com>
2014-10-11 19:56:24 +0300

    Added object basis

---
+++ Grafai/Layouts/ChartItem.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Grafai.Layouts
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ChartItem : UserControl
+    {
+        Frame layout;
+        public ChartItem(Frame layout, string name, string data, int height = 0)
+        {
+            InitializeComponent();
+            this.layout = layout;
+            chartName.Text = name;
+            var uriSource = new Uri(@"/Grafai;component/"+data, UriKind.Relative);
+            chartIcon.Source = new BitmapImage(uriSource);
+            if (height > 0)
+            {
+                chartIcon.Height = height;
+            }
+        }
+        private void changeBackground(object sender, MouseEventArgs e)
+        {
+            Main.Background = Brushes.PapayaWhip;
+        }
+
+        private void restoreBackground(object sender, MouseEventArgs e)
+        {
+            Main.Background = Brushes.WhiteSmoke;
+        }
+        private void openMainWindow(object sender, MouseEventArgs e)
+        {
+            layout.Navigate(new WorkFrame());
+            
+        }
+    }
+}
---
+++ Grafai/Layouts/HomePage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grafai.Layouts
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : UserControl
+    {
+        public struct ImageData
+        {
+            public ImageData(string name, string url)
+            {
+                this.name = name;
+                this.url = url;
+            }
+            public string name;
+            public string url;
+        }
+        Frame layout;
+        public HomePage(Frame layout)
+        {
+            InitializeComponent();
+            this.layout = layout;
+            ImageData[] menuItems = new ImageData[] { new ImageData("Linijinė diagrama","Images/Actions-office-chart-line-icon.png"),
+            new ImageData("Stulpelinė diagrama","Images/bar_chart_icon.jpg"), new ImageData("Skritulinė diagrama","Images/pie_chart.png"),
+            new ImageData("Taškinė diagrama","Images/Reports Area Chart.jpg"), new ImageData("Eilutinė diagrama","Images/Reports Scatter Chart.jpg")};
+
+            ImageData[] recentItems = new ImageData[] { new ImageData("Linijinė diagrama", "Images/Actions-office-chart-line-icon.png"), new ImageData("Eilutinė diagrama", "Images/Reports Scatter Chart.jpg"), new ImageData("Linijinė diagrama", "Images/Actions-office-chart-line-icon.png") };
+
+            InitializeComponent();
+            int counter = 0;
+            foreach (ImageData element in menuItems)
+            {
+                ChartItem Box = new ChartItem(layout, element.name, element.url);
+                MainList.Children.Add(Box);
+                Grid.SetRow(Box, counter / 3);
+                Grid.SetColumn(Box, counter % 3);
+                counter++;
+            }
+            foreach (ImageData element in recentItems)
+            {
+                ChartItem Box = new ChartItem(layout, element.name, element.url, 100);
+                RecentItems.Children.Add(Box);
+            }
+        }
+    }
+}
